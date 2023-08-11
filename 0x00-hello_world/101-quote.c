8a34bb92f78f4cd3b18a5c3a4a8f3cde58845c9e
Mahlet Seifu <akotet2123@gmail.com>
2023-08-11 04:23:24 +0300

    c programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+/**
+ *main-prints the given string
+ *
+ *Return: 1
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora \
+Korpar, 2015-10-19\n");
+
+	return (1);
+}
+
+
+
+
+
